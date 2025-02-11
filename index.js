70e141b23249273f565cf8864159eafabfa3cb69
Sayan Paul <sayanp607@gmail.com>
2025-02-11 14:01:18 +0530

    testimonial section added

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ window.onscroll = () =>{
    menuBtn.classList.remove('fa-times');
    navbar.classList.remove('active');
 }
+var swiper = new Swiper(".testimonial-slider", {
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+     delay: 2500,
+     disableOnInteraction: false,
+  },
+  breakpoints: {
+     768: { slidesPerView: 2 }, /* Show 2 slides on tablets */
+     1024: { slidesPerView: 3 } /* Show 3 slides on large screens */
+  },
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+});
+
 
 var swiper = new Swiper(".course-slider", {
    spaceBetween: 20,
@@ -124,3 +143,4 @@ document.addEventListener('DOMContentLoaded', () => {
     revealOnScroll.observe(section);
   });
 });
+
