4b09142e730fc42db0489ce99a50d3082835d712
Ayanangshu Dutta <ayanangshudutta1@gmail.com>
2025-02-16 11:59:46 +0530

    removed secret keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // get this key from emailJS
 export const config = {
-  EMAILJS_USER_ID: "MvxP-IqI_6VthN4xu",
-  EMAILJS_SERVICE_ID: "service_u2p4tfn",
-  EMAILJS_TEMPLATE_ID: "template_7csr89r",
+  EMAILJS_USER_ID: "EMAILJS_USER_ID",
+  EMAILJS_SERVICE_ID: "EMAILJS_SERVICE_ID",
+  EMAILJS_TEMPLATE_ID: "EMAILJS_TEMPLATE_ID",
   APPSCRIPT_URL: "APPSCRIPT_URL",
 };
